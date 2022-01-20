02bb012c09b4f3b47503ecfda144de2c77de4220
derekboase <boaseeingeering@gmail.com>
2022-01-20 12:38:12 -0500

    Working home model with control over the joint placements

--- ros_work/test/scripts/random_trajectory.py
+++ ros_work/test/scripts/random_trajectory.py
@@ -2,11 +2,13 @@
 
 """Randomly chooses a robot trajectory in joint space by randomly updating the current pose"""
 
+import numpy as np
 import rospy
-from trajectory_msgs.msg import JointTrajectory
-from trajectory_msgs.msg import JointTrajectoryPoint
-from random import uniform
 import time
+from trajectory_msgs.msg import JointTrajectoryPoint
+from trajectory_msgs.msg import JointTrajectory
+from std_srvs.srv import Empty
+from random import uniform
 
 
 class RandomTrajectory:
@@ -24,18 +26,25 @@ class RandomTrajectory:
                                  'j2n6s300_joint_5',
                                  'j2n6s300_joint_6']
     self.point.positions = home_joints
-    self.point.velocities = [0, 0, 0, 0, 0, 0]
-    self.point.accelerations = [0, 0, 0, 0, 0, 0]
+    # self.point.velocities = [0, 0, 0, 0, 0, 0]
+    self.point.velocities = []
+    # self.point.accelerations = [0.01, 0, 0, 0, 0, 0]
+    self.point.accelerations = []
     self.point.effort = [0, 0, 0, 0, 0, 0]
-    self.Ts = 1/4
-    self.rate = rospy.Rate(4)
+    # self.point.effort = []
+    self.Ts = 1/10.0
+    self.rate = rospy.Rate(10)
+    self.t = np.arange(5*1/self.Ts)   # 5 seconds * samples / second
+    self.joint_1_setpoints = 0.1 * self.t
+    self.idx = 1
+
 
   def moveJointHome(self):
     self.jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0)
-    self.point.time_from_start = rospy.Duration.from_sec(5.0)
+    self.point.time_from_start = rospy.Duration.from_sec(10.0)
     self.jointCmd.points.append(self.point)
     count = 0
-    while(count < 30):
+    while(count < 110):
       self.pub.publish(self.jointCmd)
       count = count + 1
       self.rate.sleep()
@@ -44,13 +53,17 @@ class RandomTrajectory:
   def randomlyUpdateJoints(self):
     self.jointCmd.points = []
     self.jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0)
-    self.point.time_from_start = rospy.Duration
-    self.current_joints[0] = self.current_joints[0] + uniform(0, 0.05)
+    self.point.time_from_start = rospy.Duration.from_sec(self.Ts*1.1)
+    self.current_joints[0] = self.joint_1_setpoints[self.idx]
     self.point.positions = self.current_joints
+    # self.point.velocities[0] = delta_joint / self.Ts
     self.jointCmd.points.append(self.point)
+    print('\n##############################################################\n')
     print(self.jointCmd)
+    print('\n##############################################################\n')
     # self.jointCmd.points.positions[0] = self.jointCmd.points.positions[0] + uniform(0, 0.5)
-    print('here')
+    self.idx = self.idx + 1
+
 
 
   def start(self):
@@ -60,46 +73,9 @@ class RandomTrajectory:
     """
     self.moveJointHome()
     while not rospy.is_shutdown():
-      # Loop while the node is alive
-      # self.pub.publish(self.pose)
-      # self.rate.sleep()
-      self.randomlyUpdateJoints()
+      # self.randomlyUpdateJoints()
       self.pub.publish(self.jointCmd)
       self.rate.sleep()
-
-
-# def moveJointHome (jointcmds, _prefix, _nbJoints):
-#   topic_name = '/' + _prefix + '/effort_joint_trajectory_controller/command'
-#   pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
-#   jointCmd = JointTrajectory()
-#   point = JointTrajectoryPoint()
-#   jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);
-#   point.time_from_start = rospy.Duration.from_sec(5)
-#   for i in range(0, _nbJoints):
-#     jointCmd.joint_names.append(_prefix +'_joint_'+str(i+1))
-#     point.positions.append(jointcmds[i])
-#     point.velocities.append(0)
-#     point.accelerations.append(0)
-#     point.effort.append(0)
-#   jointCmd.points.append(point)
-#   rate = rospy.Rate(50)
-#   count = 0
-#   while (count < 50):
-#     print(jointCmd)
-#     pub.publish(jointCmd)
-#     count = count + 1
-#     rate.sleep()
-
-  # def moveJointRelative(_currentPoints, _prefix, _nbJoints):
-  #   global Ts
-  #   topic_name = '/' + _prefix + '/effort_joint_trajectory_controller/command'
-  #   pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
-  #   jointCmd = JointTrajectory()
-  #   point = JointTrajectoryPoint()
-  #   jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0);
-  #   point.time_from_start = rospy.Duration.from_sec(1/Ts)
-  #
-  # return _currentPoitns
 
 
 if __name__ == '__main__':
@@ -109,14 +85,20 @@ if __name__ == '__main__':
     #allow gazebo to launch
     time.sleep(2)
 
-    rt = RandomTrajectory([0.0, 2.7, 1.3, 4.2, 1.4, 0.0])
-    rt.start()
-    
     # Unpause the physics
-    # rospy.wait_for_service('/gazebo/unpause_physics')
-    # unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
-    # resp = unpause_gazebo()
+    rospy.wait_for_service('/gazebo/unpause_physics')
+    unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+    resp = unpause_gazebo()
     # Move the arm to it's home position
+
+    rt = RandomTrajectory([0.0, 2.7, 1.3, 4.2, 1.4, 0.0])
+    # rt = RandomTrajectory([np.deg2rad(180),
+    #                        np.deg2rad(270),
+    #                        np.deg2rad(90),
+    #                        np.deg2rad(180),
+    #                        np.deg2rad(180),
+    #                        np.deg2rad(0)])
+    rt.start()
 
   except rospy.ROSInterruptException:
     print "program interrupted before completion"
