fcac447189fdad8729367fabf23eb7797db831e4
derekboase <boaseeingeering@gmail.com>
2022-01-27 10:30:11 -0500

    Coded the control structure for joint 1 but it's not working

--- ros_work/test/scripts/random_trajectory.py
+++ ros_work/test/scripts/random_trajectory.py
@@ -2,6 +2,7 @@
 
 """Randomly chooses a robot trajectory in joint space by randomly updating the current pose"""
 
+import matplotlib.pyplot as plt
 import numpy as np
 import rospy
 import time
@@ -18,7 +19,11 @@ def generate_SPD_matrix(n):
     :return: np array of dimensions nxn
     """
     Wc = np.random.rand(n, n)
-    return 1/2 * np.matmul(Wc, np.transpose(Wc))
+    return 1/2.0 * np.matmul(Wc, np.transpose(Wc))
+
+
+def bound(low, high, val):
+    return np.array([max(low, min(high, val))])
 
 
 class RandomTrajectory:
@@ -58,11 +63,12 @@ class RandomTrajectory:
         self.j5_traj = []
         self.j6_traj = []
 
-        self.actual_positions = np.zeros(1, 6)
-        self.actual_velocities = np.zeros(1, 6)
+        self.actual_positions = np.zeros((1, 6))
+        self.actual_velocities = np.zeros((1, 6))
 
         self.idx = 0
 
+        self.algorithm = []
 
     def trajectory_calculator(self):
         """
@@ -123,37 +129,56 @@ class RandomTrajectory:
     def signal_update(self):
         # Variables
         _N = self.end_time / self.Ts
-        zeta_actor = 1  ## THIS IS NOT A REASONABLE VALUE
-        zeta_critic = 1  ## THIS IS NOT A REASONABLE VALUE
-        Q = generate_SPD_matrix(4)
+        _zeta_actor = 0.05
+        _zeta_critic = 0.1
+        Q = generate_SPD_matrix(3)
         R = generate_SPD_matrix(1)
         delta_conv, window_conv = 1e-2, _N/10
 
         _error_1 = np.zeros((3, 1))  # Step 1.1 for Joint 1 Position
         _Wc_1 = generate_SPD_matrix(4)  # Step 1.2 for Joint 1 Position
-        _Wa_1 = -np.matmul(1/_Wc_1[3][3], _Wc_1[3][0:3])  # Step 1.3 for Joint 1 Position
+        _Wa_1 = -1/_Wc_1[3][3]*_Wc_1[3][0:3]  # Step 1.3 for Joint 1 Position
         # Step 2 for Joint 1 Position is done in the setup self.j1_traj at the next index
         _k, _weights_conv = 0, False  # Step 3 and 4 for Joint 1 Position
-        while _k < _N and not _weights_conv:
+        # while _k < _N and not _weights_conv:
+        while _k < _N:
             # Calculate the control signal: Step 6 for Joint 1 Position
-            u_pi = np.matmul(_Wa_1, _error_1)
+            _u_hat = bound(np.pi/2.0, np.pi/2.0, np.matmul(_Wa_1, _error_1))
             next_tar = np.array(self.current_joints)
-            next_tar[0] += u_pi
+            print('For k={0}, _u_hat = {1}'.format(_k, _u_hat))
+            next_tar[0] += _u_hat
+            print(next_tar[0])
             self.update_target(next_tar)
+            self.algorithm.append(self.actual_positions[0])
             self.pub.publish(self.jointCmd)
             self.rate.sleep()
+
             # Find V and U: Step 8 for Joint 1 Position
-            Eu_concat = np.concatenate((_error_1, u_pi.reshape(-1, 1)), axis=0)
+            Eu_concat = np.concatenate((_error_1, _u_hat.reshape(-1, 1)), axis=0)
             Eu_transpose = np.transpose(Eu_concat)
-            V = 1/2 * np.matmul(np.matmul(Eu_transpose, _Wc_1), Eu_concat)
+            V_k = 1/2.0 * np.matmul(np.matmul(Eu_transpose, _Wc_1), Eu_concat)
             E_transpose = np.transpose(_error_1)
-            U = 1/2 * (np.matmul(np.matmul(E_transpose, Q), _error_1) + np.square(u_pi)*R)
-            # Get E(k + 1)
+            U_k = 1/2.0 * (np.matmul(np.matmul(E_transpose, Q), _error_1) + np.square(_u_hat)*R)
+
+            # Get E(k + 1), u_hat and V(k_1): Step 9 for Joint 1 Position
             _error_1[2] = _error_1[1]
             _error_1[1] = _error_1[0]
             _error_1[0] = self.j1_traj[_k + 1] - self.actual_positions[0]
+            _u_hat = np.matmul(_Wa_1, _error_1)
+            Eu_concat_k1 = np.concatenate((_error_1, _u_hat.reshape(-1, 1)), axis=0)
+            Eu_transpose_k1 = np.transpose(Eu_concat)
+            _V_k1 = 1/2.0*np.matmul(np.matmul(Eu_transpose_k1, _Wc_1), Eu_concat_k1)
+            _V_tilde = U_k + V_k
+            _u_tilde = -1/_Wc_1[3][3]*_Wc_1[3][0:3]
+            _epsilon_critic, _epsilon_actor = 1/2 * np.square(V_k - _V_tilde), 1/2*np.square(_u_hat - _u_tilde)
 
-            pass
+            # Update critic weights: Step 11 for Joint 1 Position
+            _Wc_1 = _Wc_1 - _zeta_critic * _epsilon_critic*np.matmul(Eu_concat, Eu_transpose)
+
+            # Update actor weights: Step 12 for Joint 1 Position
+            _Wa_1 = _Wa_1 - _zeta_actor*_epsilon_actor*np.transpose(_error_1)
+            _k += 1
+            print'ERROR:\n{0}'.format(_error_1)
         return _Wc_1, _Wa_1
 
 
@@ -164,29 +189,44 @@ class RandomTrajectory:
         """
         rospy.Subscriber('/j2s6s300/joint_states', JointState, self.actual_values)
         self.moveJointHome()
-        while not rospy.is_shutdown():
-            # self.nominal_trajectory()
-            pass
+        print("******************************************************************")
+        print("\t\t\tNominal Motion")
+        print("******************************************************************")
+        self.nominal_trajectory()
+        print("******************************************************************")
+        print("\t\t\tAlgorithm Motion")
+        print("******************************************************************")
+        self.signal_update()
+        t = np.arange(0, self.end_time + self.Ts, step=self.Ts)
+        plt.plot(t, self.j1_traj)
+        plt.plot(t[1:], self.algorithm)
+        plt.legend(['nominal', 'actual'])
+        plt.show()
+
+        # while not rospy.is_shutdown():
+        #     # self.nominal_trajectory()
+        #     self.signal_update()
+        #     pass
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('move_robot_using_trajectory_msg')
         # prefix, nbJoints, nbfingers = 'j2s6s300', 6, 3
-        #allow gazebo to launch
+        # allow gazebo to launch
         time.sleep(2)
 
         # Unpause the physics
         # rospy.wait_for_service('/gazebo/unpause_physics')
         # unpause_gazebo = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         # resp = unpause_gazebo()
-          # Move the arm to it's home position
 
-        # rt = RandomTrajectory([0.0, 2.7, 1.3, 4.2, 1.4, 0.0])
-        rt = RandomTrajectory([0.0, 2.0, 1.3, 4.2, 1.4, 0.0])
+        rt = RandomTrajectory([0.0, 2.7, 1.3, 4.2, 1.4, 0.0])
+        # rt = RandomTrajectory([0.0, 2.0, 1.3, 4.2, 1.4, 0.0])
         # rt = RandomTrajectory(np.deg2rad([180, 270, 90, 270, 270, 270]))
         rt.trajectory_calculator()
         rt.start()
 
     except rospy.ROSInterruptException:
-        print "program interrupted before completion"
+        print"program interrupted before completion"
+        
