054f15c050a5ff63d4f2471753c771f711eb94b7
derekboase <boaseeingeering@gmail.com>
2022-01-31 09:20:51 -0500

    algorithm implemented, but not working. Missing convergence loop

--- ros_work/test/scripts/random_trajectory.py
+++ ros_work/test/scripts/random_trajectory.py
@@ -18,8 +18,8 @@ def generate_SPD_matrix(n):
     :param n: Number of dimensions for the symmetric positive definite matrix
     :return: np array of dimensions nxn
     """
-    Wc = np.random.rand(n, n)
-    return 1/2.0 * np.matmul(Wc, np.transpose(Wc))
+    A = np.random.rand(n, n)
+    return 1/2.0 * np.matmul(A, np.transpose(A))
 
 
 def bound(low, high, val):
@@ -57,11 +57,7 @@ class RandomTrajectory:
 
         ## IMPROVE THIS BY MAKING IT ONE LARGE ARRAY CAllED self.traj with dimensions
         self.j1_traj = []
-        self.j2_traj = []
-        self.j3_traj = []
-        self.j4_traj = []
-        self.j5_traj = []
-        self.j6_traj = []
+
 
         self.actual_positions = np.zeros((1, 6))
         self.actual_velocities = np.zeros((1, 6))
@@ -78,11 +74,13 @@ class RandomTrajectory:
         # Trajectory points
         _time = np.arange(0, self.end_time + self.Ts, step=self.Ts)
         _tau = 3
+        _amplitude = np.pi
         for t in _time:
-            if t <= self.end_time/2:
-                self.j1_traj.append(np.pi * (1 - np.exp(-_tau * t / np.pi)))
-            else:
-                self.j1_traj.append(np.pi * np.exp(-_tau * (t - 5) / np.pi))
+            # if t <= self.end_time/2:
+            #     self.j1_traj.append(_amplitude * (1 - np.exp(-_tau * t / np.pi)))
+            # else:
+            #     self.j1_traj.append(_amplitude * np.exp(-_tau * (t - self.end_time/2) / np.pi))
+            self.j1_traj.append(np.pi/2)
 
     def actual_values(self, real_joint_angles):
         self.actual_positions = np.array(real_joint_angles.position[0:6])
@@ -120,67 +118,70 @@ class RandomTrajectory:
         self.jointCmd.points = []
         self.jointCmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0)
         self.point.time_from_start = rospy.Duration.from_sec(self.Ts)
-        self.point.velocities = ((next_targets - np.array(self.current_joints)) / self.Ts).tolist()
-        self.point.accelerations = (np.array(self.point.velocities) / self.Ts).tolist()
-        self.current_joints = next_targets.tolist()
-        self.point.positions = self.current_joints
+        # self.point.velocities = ((next_targets - np.array(self.current_joints)) / self.Ts).tolist()
+        # self.point.accelerations = (np.array(self.point.velocities) / self.Ts).tolist()
+        # self.current_joints = next_targets.tolist()
+        # self.point.positions = self.current_joints
+        self.point.positions = next_targets.tolist()
         self.jointCmd.points.append(self.point)
 
     def signal_update(self):
-        # Variables
+        # Initialization of algorithm constants
         _N = self.end_time / self.Ts
-        _zeta_actor = 0.05
-        _zeta_critic = 0.1
+        _zeta_actor = 0.01  # Choose the actor to adapt more slowly than the critic
+        _zeta_critic = 0.05
         Q = generate_SPD_matrix(3)
         R = generate_SPD_matrix(1)
         delta_conv, window_conv = 1e-2, _N/10
 
-        _error_1 = np.zeros((3, 1))  # Step 1.1 for Joint 1 Position
-        _Wc_1 = generate_SPD_matrix(4)  # Step 1.2 for Joint 1 Position
-        _Wa_1 = -1/_Wc_1[3][3]*_Wc_1[3][0:3]  # Step 1.3 for Joint 1 Position
-        # Step 2 for Joint 1 Position is done in the setup self.j1_traj at the next index
-        _k, _weights_conv = 0, False  # Step 3 and 4 for Joint 1 Position
-        # while _k < _N and not _weights_conv:
-        while _k < _N:
+        _k, _weights_conv = 0, False  # Index and convergence flag
+
+        _E_k = np.zeros((3, 1))  # shape(3, 1)
+        _E_k1 = np.zeros((3, 1))  # shape(3, 1)
+
+        _Wc_1 = generate_SPD_matrix(4)  # shape(4, 4)
+        _Wa_1 = (-1/_Wc_1[3][3]*_Wc_1[3][0:3]).reshape(1, 3)  # shape(1, 3)
+        # _Wa_1 = np.zeros((1, 3))  # shape(1, 3)
+
+        next_tar = np.array(self.current_joints)  # load the next target variable with the current joints
+        while _k < _N and not _weights_conv:
             # Calculate the control signal: Step 6 for Joint 1 Position
-            _u_hat = bound(np.pi/2.0, np.pi/2.0, np.matmul(_Wa_1, _error_1))
-            next_tar = np.array(self.current_joints)
-            print('For k={0}, _u_hat = {1}'.format(_k, _u_hat))
-            next_tar[0] += _u_hat
-            print(next_tar[0])
+            _u_hat = bound(-0.35, 0.35, float(np.matmul(_Wa_1, _E_k)))  # shape(1,)
+            next_tar[0] += _u_hat  # shape(1,)
             self.update_target(next_tar)
-            self.algorithm.append(self.actual_positions[0])
-            self.pub.publish(self.jointCmd)
+            self.algorithm.append(self.actual_positions[0])  # For the sole purpose of
+            self.pub.publish(self.jointCmd)  # Publish the
             self.rate.sleep()
 
             # Find V and U: Step 8 for Joint 1 Position
-            Eu_concat = np.concatenate((_error_1, _u_hat.reshape(-1, 1)), axis=0)
+            Eu_concat = np.concatenate((_E_k, _u_hat.reshape(-1, 1)), axis=0)
             Eu_transpose = np.transpose(Eu_concat)
-            V_k = 1/2.0 * np.matmul(np.matmul(Eu_transpose, _Wc_1), Eu_concat)
-            E_transpose = np.transpose(_error_1)
-            U_k = 1/2.0 * (np.matmul(np.matmul(E_transpose, Q), _error_1) + np.square(_u_hat)*R)
+            _V_k = 1/2.0 * np.matmul(np.matmul(Eu_transpose, _Wc_1), Eu_concat)
+            E_transpose = np.transpose(_E_k)
+            _U_k = 1/2.0 * (np.matmul(np.matmul(E_transpose, Q), _E_k) + _u_hat*R*_u_hat)
 
             # Get E(k + 1), u_hat and V(k_1): Step 9 for Joint 1 Position
-            _error_1[2] = _error_1[1]
-            _error_1[1] = _error_1[0]
-            _error_1[0] = self.j1_traj[_k + 1] - self.actual_positions[0]
-            _u_hat = np.matmul(_Wa_1, _error_1)
-            Eu_concat_k1 = np.concatenate((_error_1, _u_hat.reshape(-1, 1)), axis=0)
-            Eu_transpose_k1 = np.transpose(Eu_concat)
-            _V_k1 = 1/2.0*np.matmul(np.matmul(Eu_transpose_k1, _Wc_1), Eu_concat_k1)
-            _V_tilde = U_k + V_k
-            _u_tilde = -1/_Wc_1[3][3]*_Wc_1[3][0:3]
-            _epsilon_critic, _epsilon_actor = 1/2 * np.square(V_k - _V_tilde), 1/2*np.square(_u_hat - _u_tilde)
+            _E_k1[2] = _E_k[1]
+            _E_k1[1] = _E_k[0]
+            _E_k1[0] = self.j1_traj[_k + 1] - self.actual_positions[0]
+            _u_hat_k1 = bound(-0.35, 0.35, float(np.matmul(_Wa_1, _E_k1)))  # shape(1,)
+            _Z = np.concatenate((_E_k1, _u_hat_k1.reshape(-1, 1)), axis=0)
+            _Z_trans = np.transpose(_Z)
+            _V_k1 = 1/2.0*np.matmul(np.matmul(_Z_trans, _Wc_1), _Z)
 
             # Update critic weights: Step 11 for Joint 1 Position
-            _Wc_1 = _Wc_1 - _zeta_critic * _epsilon_critic*np.matmul(Eu_concat, Eu_transpose)
+            temp = _zeta_critic*(_V_k - (_U_k + _V_k1))
+            # temp = _zeta_critic*(_V_k - (_U_k + _V_k1))
+            _Wc_1 -= temp*np.matmul(_Z, _Z_trans)
 
             # Update actor weights: Step 12 for Joint 1 Position
-            _Wa_1 = _Wa_1 - _zeta_actor*_epsilon_actor*np.transpose(_error_1)
-            _k += 1
-            print'ERROR:\n{0}'.format(_error_1)
-        return _Wc_1, _Wa_1
+            _Wa_1 -= _zeta_actor*(np.matmul(_Wa_1, _E_k) - (-1/_Wc_1[3][3]*np.matmul(_Wc_1[3][0:3], _E_k)))*E_transpose
 
+            # Updates
+            _E_k = _E_k1
+            _k += 1
+
+        return _Wc_1, _Wa_1
 
     def start(self):
         """(self) -> None
@@ -189,10 +190,10 @@ class RandomTrajectory:
         """
         rospy.Subscriber('/j2s6s300/joint_states', JointState, self.actual_values)
         self.moveJointHome()
-        print("******************************************************************")
-        print("\t\t\tNominal Motion")
-        print("******************************************************************")
-        self.nominal_trajectory()
+        # print("******************************************************************")
+        # print("\t\t\tNominal Motion")
+        # print("******************************************************************")
+        # self.nominal_trajectory()
         print("******************************************************************")
         print("\t\t\tAlgorithm Motion")
         print("******************************************************************")
@@ -229,4 +230,3 @@ if __name__ == '__main__':
 
     except rospy.ROSInterruptException:
         print"program interrupted before completion"
-        
